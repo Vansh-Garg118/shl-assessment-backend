05a98e1adff49cd01561d68cf871515175e6dee9
Vansh Garg <vanshgarg25804@gmail.com>
2025-04-06 18:20:31 +0530

    Added toml file for railway deployment

--- app.py
+++ app.py
@@ -84,9 +84,5 @@ def recommend():
     return jsonify(recommendations)
 
 # Run the API server locally
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
-
+    app.run(debug=True)
